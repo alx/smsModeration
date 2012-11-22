ab1cdf1f755fff8e87e0e458fcf2488c5c7b1c48
Alexandre Girard <git@alexgirard.com>
2012-11-22 18:21:23 +0100

    remove holon.zip and finish some requests on smssync

--- smssync.rb
+++ smssync.rb
@@ -1,13 +1,16 @@
 require 'rubygems'
 require 'sinatra'
+require 'json'
 
 gem 'dm-core'
 gem 'dm-timestamps'
 gem 'dm-migrations'
+gem 'dm-serializer'
 
 require 'dm-core'
 require 'dm-timestamps'
 require 'dm-migrations'
+require 'dm-serializer'
 
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/smssync.db")
 
@@ -15,13 +18,27 @@ class Message
   include DataMapper::Resource
 
   property :id, Serial
-  property :text, String
+  property :msg, Text
+  property :tel, String
 
   property :created_at, DateTime
   property :validated_at, DateTime
 
-  property :is_valid, Boolean
-  property :is_example, Boolean
+  property :is_valid, Boolean, :default => false
+  property :is_example, Boolean, :default => false
+
+  def to_json(args)
+    output = {
+      :id => self.id,
+      :msg => self.msg,
+      :date => self.created_at,
+      :is_valid => self.is_valid,
+      :is_example => self.is_example
+    }
+    output[:phone_messages] = Message.all(:tel => self.tel).size
+    output[:phone_valid_messages] = Message.all(:tel => self.tel, :is_valid => true).size
+    output
+  end
 end
 
 class Bucket
@@ -29,7 +46,11 @@ class Bucket
 
   property :id, Serial
   property :created_at, DateTime
+
+  property :version, Integer
 end
+
+DataMapper.finalize
 
 configure do
   set :public_folder, 'yeoman/dist/'
@@ -47,7 +68,7 @@ get '/' do
   end
 
   if params['device'] == 'gateway'
-    Message.create :text => params['text']
+    Message.create :msg => params['text'], :tel => params['tel']
   else
     send_file File.expand_path('yeoman/dist/index.html', '.')
   end
@@ -55,15 +76,39 @@ get '/' do
 end
 
 get '/selected_messages' do
-
 end
 
 get '/examples.json' do
+  return Message.all(:is_example => true).to_json
+end
 
+get '/new_messages.json' do
+  return Message.all(:validated_at => nil).to_json
 end
 
 post '/accept/:message' do
+  msg = Message.get(:message)
+  if msg
+    msg.update(:is_valid => true, :validated_at => Time.now)
+    return {:status => "ok"}.to_json
+  else
+    not_found
+  end
 end
 
 post '/reject/:message' do
+  msg = Message.get(:message)
+  if msg
+    msg.update(:is_valid => false, :validated_at => Time.now)
+    return {:status => "ok"}.to_json
+  else
+    not_found
+  end
+end
+
+get '/create_dummies' do
+  Message.create(:msg => "text1", :tel => "001122334455")
+  Message.create(:msg => "text2", :tel => "001122334455")
+  Message.create(:msg => "text3", :tel => "001122334455")
+  Message.create(:msg => "text4", :tel => "001122334455")
 end
