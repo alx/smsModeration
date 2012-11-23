f54c6e16b3e7453eefa18fcc0534e1bdf23de971
Alexandre Girard <git@alexgirard.com>
2012-11-23 17:42:52 +0100

    mostly working

---
+++ README.md
@@ -0,0 +1,5 @@
+# Install
+
+```
+gem install sinatra dm-core dm-migrations dm-timestamps dm-serializer dm-sqlite-adapter
+```
--- smssync.rb
+++ smssync.rb
@@ -12,6 +12,10 @@ require 'dm-timestamps'
 require 'dm-migrations'
 require 'dm-serializer'
 
+# ======
+# Models
+# ======
+
 DataMapper.setup(:default, "sqlite3:///#{Dir.pwd}/smssync.db")
 
 class Message
@@ -26,14 +30,19 @@ class Message
 
   property :is_valid, Boolean, :default => false
   property :is_example, Boolean, :default => false
+  property :is_favorite, Boolean, :default => false
+
+  has n, :selections, :through => Resource
 
   def to_json(args)
     output = {
       :id => self.id,
       :msg => self.msg,
       :date => self.created_at,
+      :hours => self.created_at.strftime("%H:%M"),
       :is_valid => self.is_valid,
-      :is_example => self.is_example
+      :is_example => self.is_example,
+      :is_favorite => self.is_favorite
     }
     output[:phone_messages] = Message.all(:tel => self.tel).size
     output[:phone_valid_messages] = Message.all(:tel => self.tel, :is_valid => true).size
@@ -41,31 +50,41 @@ class Message
   end
 end
 
-class Bucket
+class Selection
   include DataMapper::Resource
 
   property :id, Serial
   property :created_at, DateTime
 
-  property :version, Integer
+  has n, :messages, :through => Resource
 end
 
 DataMapper.finalize
 
+# init database if not already created
+begin
+  @selection = Selection.first
+rescue
+  DataMapper.auto_migrate!
+  @selection = Selection.new
+  @selection.save
+end
+
+# =============
+# Configuration
+# =============
+
 configure do
-  set :public_folder, 'yeoman/dist/'
+  set :public_folder, 'yeoman/app/'
   set :static, true
 end
 
-get '/' do
 
-  begin
-    @bucket = Bucket.first
-  rescue
-    DataMapper.auto_migrate!
-    @bucket = Bucket.new
-    @bucket.save
-  end
+# ====
+# Main
+# ====
+
+get '/' do
 
   if params['device'] == 'gateway'
     Message.create :msg => params['text'], :tel => params['tel']
@@ -75,21 +94,55 @@ get '/' do
 
 end
 
-get '/selected_messages' do
+# =============
+# Messages JSON
+# =============
+
+get '/selected.json' do
+  return Selection.last.messages.to_json
 end
 
 get '/examples.json' do
   return Message.all(:is_example => true).to_json
 end
 
-get '/new_messages.json' do
+get '/recents.json' do
   return Message.all(:validated_at => nil).to_json
+end
+
+get '/favorites.json' do
+  return Message.all(:is_favorite => true).to_json
+end
+
+# ================
+# Messages Actions
+# ================
+
+post '/select/:message' do
+  msg = Message.get(:message)
+  if msg
+    Selection.last.messages << msg
+    return {:status => "ok"}.to_json
+  else
+    not_found
+  end
+end
+
+post '/favorite/:message' do
+  msg = Message.get(:message)
+  if msg
+    msg.update(:is_favorite => true)
+    return {:status => "ok"}.to_json
+  else
+    not_found
+  end
 end
 
 post '/accept/:message' do
   msg = Message.get(:message)
   if msg
     msg.update(:is_valid => true, :validated_at => Time.now)
+    Selection.last.messages << msg
     return {:status => "ok"}.to_json
   else
     not_found
@@ -105,6 +158,23 @@ post '/reject/:message' do
     not_found
   end
 end
+
+# =================
+# Selection Actions
+# =================
+
+get '/messages.txt' do
+  messages = Selection.last.messages
+  return "&vMessageListe=#{messages.join('|')}\n&vTxt_version=#{Selection.last.id}"
+end
+
+post '/selection' do
+  Selection.create
+end
+
+# ====
+# Misc
+# ====
 
 get '/create_dummies' do
   Message.create(:msg => "text1", :tel => "001122334455")
